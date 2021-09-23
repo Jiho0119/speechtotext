3d1de48b09613821084cc2bfd759af7f3b1a68f4
unknown <jihohan0119@outlook.com>
2021-09-22 18:11:34 -0700

    initial commit

---
+++ app/src/main/java/com/speechtotext/MainActivity.java
@@ -0,0 +1,34 @@
+package com.speechtotext;
+
+import static android.Manifest.permission.INTERNET;
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+import static android.Manifest.permission.RECORD_AUDIO;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Initialize SpeechSDK and request required permissions.
+        try {
+            // a unique number within the application to allow
+            // correlating permission request responses with the request.
+            int permissionRequestId = 5;
+
+            // Request permissions needed for speech recognition
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{RECORD_AUDIO, INTERNET, READ_EXTERNAL_STORAGE}, permissionRequestId);
+        }
+        catch(Exception ex) {
+            Log.e("SpeechSDK", "could not init sdk, " + ex.toString());
+        }
+
+    }
+}
