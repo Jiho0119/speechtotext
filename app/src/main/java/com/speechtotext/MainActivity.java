f931554280a6513135da19866c6db8f9da9dee07
Min Lee <minlee1@microsoft.com>
2021-09-26 14:05:38 -0700

    ml

--- app/src/main/java/com/speechtotext/MainActivity.java
+++ app/src/main/java/com/speechtotext/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 import com.google.android.material.tabs.TabLayoutMediator;
 import com.speechtotext.databinding.ActivityMainBinding;
@@ -19,6 +20,7 @@ public class MainActivity extends FragmentActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        viewModel = new ViewModelProvider(this, new ViewModelProvider.NewInstanceFactory()).get(PlannerViewModel.class);
 
         mAdapter = new MyAdapter(this);
 
